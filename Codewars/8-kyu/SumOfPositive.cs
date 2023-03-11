e4a8a8e6c61098c3e40d864ed076ba89bcbe1dfd
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-10 23:11:21 -0300

    feat: SumOfPositive

---
+++ Codewars/8-kyu/SumOfPositive.cs
@@ -0,0 +1,18 @@
+﻿namespace Codewars._8_kyu
+{
+  public static class SumOfPositive
+  {
+    public static int Handle(int[] arr)
+    {
+      int result = 0;
+      for (int i = 0; i < arr.Length; i++)
+      {
+        if (arr[i] > 0)
+        {
+          result += arr[i];
+        }
+      }
+      return result;
+    }
+  }
+}
