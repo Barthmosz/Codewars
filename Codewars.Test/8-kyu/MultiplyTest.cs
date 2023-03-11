02b99fe921fd959e4b9faed76a1ee3437396db16
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-10 23:05:19 -0300

    test: Multiply

---
+++ Codewars.Test/8-kyu/MultiplyTest.cs
@@ -0,0 +1,19 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class MultiplyTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(Multiply.Handle(2, 1), Is.EqualTo(2));
+        Assert.That(Multiply.Handle(2, 4), Is.EqualTo(8));
+      });
+    }
+  }
+}
