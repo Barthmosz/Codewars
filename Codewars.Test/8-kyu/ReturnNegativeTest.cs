2033606dfbd2574d6a995dde4cca3bfee5b3632e
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-10 23:14:20 -0300

    test: ReturnNegative

---
+++ Codewars.Test/8-kyu/ReturnNegativeTest.cs
@@ -0,0 +1,16 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class ReturnNegativeTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.That(ReturnNegative.Handle(42), Is.EqualTo(-42));
+      Assert.That(ReturnNegative.Handle(-5), Is.EqualTo(-5));
+    }
+  }
+}
