8d5436b52eaeea0e65133f667588a62c8b56c598
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-10 23:02:43 -0300

    test: EvenOrOdd

---
+++ Codewars.Test/8-kyu/EvenOrOddTest.cs
@@ -0,0 +1,22 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class EvenOrOddTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(EvenOrOdd.Handle(2), Is.EqualTo("Even"));
+        Assert.That(EvenOrOdd.Handle(1), Is.EqualTo("Odd"));
+        Assert.That(EvenOrOdd.Handle(0), Is.EqualTo("Even"));
+        Assert.That(EvenOrOdd.Handle(7), Is.EqualTo("Odd"));
+        Assert.That(EvenOrOdd.Handle(-1), Is.EqualTo("Odd"));
+      });
+    }
+  }
+}
