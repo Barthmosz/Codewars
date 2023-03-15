8bf3681273815204888137b241b834506031faa0
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-14 23:29:01 -0300

    feat: ReversedStrings

---
+++ Codewars/8-kyu/ReversedStrings.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+  public static class ReversedStrings
+  {
+    public static string Handle(string str)
+    {
+      string result = "";
+
+      for (int i = str.Length; i > 0; i--)
+      {
+        result += str[i - 1];
+      }
+
+      return result;
+    }
+  }
+}
