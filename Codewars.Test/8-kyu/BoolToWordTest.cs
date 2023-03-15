e315e62a6ade5d30498eda6f3089f42cc60a48fc
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-14 23:41:20 -0300

    test: BoolToWord

---
+++ Codewars.Test/8-kyu/BoolToWordTest.cs
@@ -0,0 +1,19 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class BoolToWordTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(BoolToWord.Handle(true), Is.EqualTo("Yes"));
+        Assert.That(BoolToWord.Handle(false), Is.EqualTo("No"));
+      });
+    }
+  }
+}
