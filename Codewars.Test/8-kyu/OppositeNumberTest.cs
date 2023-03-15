ec56ee0234c3b1dbb61ffc146a29b1288d4e8ae5
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-14 23:47:52 -0300

    test: OppositeNumber

---
+++ Codewars.Test/8-kyu/OppositeNumberTest.cs
@@ -0,0 +1,15 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class OppositeNumberTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.That(OppositeNumber.Handle(-1), Is.EqualTo(1));
+    }
+  }
+}
