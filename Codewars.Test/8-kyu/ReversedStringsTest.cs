a9ba665f7fe57fb1e781b4806c6cf2c938306ce5
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-14 23:29:06 -0300

    test: ReversedStrings

---
+++ Codewars.Test/8-kyu/ReversedStringsTest.cs
@@ -0,0 +1,15 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class ReversedStringsTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.That(ReversedStrings.Handle("world"), Is.EqualTo("dlrow"));
+    }
+  }
+}
