d7ec795fc9bbae2764983112947981c4fd060a96
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-14 23:45:44 -0300

    test: NumberToString

---
+++ Codewars.Test/8-kyu/NumberToStringTest.cs
@@ -0,0 +1,27 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class NumberToStringTest
+  {
+    private static IEnumerable<TestCaseData> TestCases
+    {
+      get
+      {
+        yield return new TestCaseData(67).Returns("67");
+        yield return new TestCaseData(79585).Returns("79585");
+        yield return new TestCaseData(1 + 2).Returns("3");
+        yield return new TestCaseData(1 - 2).Returns("-1");
+      }
+    }
+
+    [Test, TestCaseSource(nameof(TestCases))]
+    public string HandleTest(int num)
+    {
+      return NumberToString.Handle(num);
+    }
+  }
+}
