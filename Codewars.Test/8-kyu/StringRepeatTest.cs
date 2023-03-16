df6183ff509447ffc4b89bdb8f4dc7ef6b1f7b9d
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-15 22:32:22 -0300

    test: StringRepeat

---
+++ Codewars.Test/8-kyu/StringRepeatTest.cs
@@ -0,0 +1,20 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class StringRepeatTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(StringRepeat.Handle(3, "*"), Is.EqualTo("***"));
+        Assert.That(StringRepeat.Handle(5, "#"), Is.EqualTo("#####"));
+        Assert.That(StringRepeat.Handle(2, "ha "), Is.EqualTo("ha ha "));
+      });
+    }
+  }
+}
