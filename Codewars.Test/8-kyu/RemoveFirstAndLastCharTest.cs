547eb702bb60f539937434c992a85c5503fb988f
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-15 22:28:58 -0300

    test: RemoveFirstAndLastChar

---
+++ Codewars.Test/8-kyu/RemoveFirstAndLastCharTest.cs
@@ -0,0 +1,22 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+  [TestFixture]
+  public class RemoveFirstAndLastCharTest
+  {
+    [Test]
+    public void HandleTest()
+    {
+      Assert.Multiple(() =>
+      {
+        Assert.That(RemoveFirstAndLastChat.Handle("eloquent"), Is.EqualTo("loquen"));
+        Assert.That(RemoveFirstAndLastChat.Handle("country"), Is.EqualTo("ountr"));
+        Assert.That(RemoveFirstAndLastChat.Handle("person"), Is.EqualTo("erso"));
+        Assert.That(RemoveFirstAndLastChat.Handle("place"), Is.EqualTo("lac"));
+        Assert.That(RemoveFirstAndLastChat.Handle("ok"), Is.EqualTo(""));
+      });
+    }
+  }
+}
