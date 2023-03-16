33f0a5f25c980ca9d2b9847d949cf94916ab0be5
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-15 22:28:51 -0300

    feat: RemoveFirstAndLastChar

---
+++ Codewars/8-kyu/RemoveFirstAndLastChar.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+  public static class RemoveFirstAndLastChat
+  {
+    public static string Handle(string s)
+    {
+      string result = "";
+
+      for (int i = 1; i < s.Length - 1; i++)
+      {
+        result += s[i];
+      }
+
+      return result;
+    }
+  }
+}
