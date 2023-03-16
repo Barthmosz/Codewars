d14319ac9088c0058ee9c8d820cb6a0be06f7cef
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-15 22:32:18 -0300

    feat: StringRepeat

---
+++ Codewars/8-kyu/StringRepeat.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+  public static class StringRepeat
+  {
+    public static string Handle(int n, string s)
+    {
+      string result = "";
+
+      for (int i = 0; i < n; i++)
+      {
+        result += s;
+      }
+
+      return result;
+    }
+  }
+}
