8618d4d0004ae9b17685f8461a16aac634da3958
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-16 23:57:57 -0300

    feat: SquareSum

---
+++ Codewars/8-kyu/SquareSum.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+    public static class SquareSum
+    {
+        public static int Handle(int[] numbers)
+        {
+            int result = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += (numbers[i] * numbers[i]);
+            }
+
+            return result;
+        }
+    }
+}
