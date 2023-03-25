bb73f1e82a526037c787700d9f804291055bdafd
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-24 22:09:19 -0300

    test: RemoveStringSpaces

---
+++ Codewars.Test/8-kyu/RemoveStringSpaces.cs
@@ -0,0 +1,20 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class RemoveStringSpacesTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(RemoveStringSpaces.Handle("8 j 8   mBliB8g  imjB8B8  jl  B"), Is.EqualTo("8j8mBliB8gimjB8B8jlB"));
+                Assert.That(RemoveStringSpaces.Handle("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"), Is.EqualTo("88Bifk8hB8BB8BBBB888chl8BhBfd"));
+                Assert.That(RemoveStringSpaces.Handle("8aaaaa dddd r     "), Is.EqualTo("8aaaaaddddr"));
+            });
+        }
+    }
+}
