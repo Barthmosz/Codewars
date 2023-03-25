76a7468bd7ebe70fe5e7de07e71cc5126c1af858
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-24 22:09:15 -0300

    feat: RemoveStringSpaces

---
+++ Codewars/8-kyu/RemoveStringSpaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Codewars._8_kyu
+{
+    public static class RemoveStringSpaces
+    {
+        public static string Handle(string input)
+        {
+            string result = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString() != " ")
+                {
+                    result += input[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
