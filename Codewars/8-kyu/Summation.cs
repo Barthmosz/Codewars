a87f523b0834fb80459d497ea2a2ac3d45a8ad7e
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-24 21:59:13 -0300

    feat: Summation

---
+++ Codewars/8-kyu/Summation.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+    public static class Summation
+    {
+        public static int Handle(int num)
+        {
+            int result = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+    }
+}
