1a18d299b095e337f880342f6ce1d375543bb62e
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-24 21:59:19 -0300

    test: Summation

---
+++ Codewars.Test/8-kyu/Summation.cs
@@ -0,0 +1,22 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class SummationTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(Summation.Handle(1), Is.EqualTo(1));
+                Assert.That(Summation.Handle(8), Is.EqualTo(36));
+                Assert.That(Summation.Handle(22), Is.EqualTo(253));
+                Assert.That(Summation.Handle(100), Is.EqualTo(5050));
+                Assert.That(Summation.Handle(213), Is.EqualTo(22791));
+            });
+        }
+    }
+}
