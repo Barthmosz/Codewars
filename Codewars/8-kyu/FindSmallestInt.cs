263e85c855c90efd46108f2df3647a56a0b0e9a2
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-29 22:54:15 -0300

    feat: FindSmallestInt

---
+++ Codewars/8-kyu/FindSmallestInt.cs
@@ -0,0 +1,20 @@
+﻿namespace Codewars._8_kyu
+{
+    public static class FindSmallestInt
+    {
+        public static int Handle(int[] args)
+        {
+            int result = args[0];
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (result > args[i])
+                {
+                    result = args[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
