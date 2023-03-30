abaeac370efbc43fa2131112b529864e9b43672e
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-29 22:54:21 -0300

    test: FindSmallestInt

---
+++ Codewars.Test/8-kyu/FindSmallestInt.cs
@@ -0,0 +1,17 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class FindSmallestIntTest
+    {
+        [Test]
+        [TestCase(new int[] { 78, 56, 232, 12, 11, 43 }, ExpectedResult = 11)]
+        [TestCase(new int[] { 78, 56, -2, 12, 8, -33 }, ExpectedResult = -33)]
+        public int HandleTest(int[] args)
+        {
+            return FindSmallestInt.Handle(args);
+        }
+    }
+}
