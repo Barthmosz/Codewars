49900f8dca27db6480baab05fe6f7ecbe0d1a3fb
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-05 22:34:06 -0300

    feat: CountSheeps

---
+++ Codewars/8-kyu/CountSheeps.cs
@@ -0,0 +1,20 @@
+﻿namespace Codewars._8_kyu
+{
+    public static class CountSheeps
+    {
+        public static int Handle(bool[] sheeps)
+        {
+            int result = 0;
+
+            for (int i = 0; i < sheeps.Length; i++)
+            {
+                if (sheeps[i])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
