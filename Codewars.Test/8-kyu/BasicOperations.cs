902f04bfed7ada855d39bdc5088475a16e9a95a4
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-05 23:11:52 -0300

    test: BasicOperations

---
+++ Codewars.Test/8-kyu/BasicOperations.cs
@@ -0,0 +1,21 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class BasicOperationsTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(BasicOperations.Handle('+', 4, 7), Is.EqualTo(11));
+                Assert.That(BasicOperations.Handle('-', 15, 18), Is.EqualTo(-3));
+                Assert.That(BasicOperations.Handle('*', 5, 5), Is.EqualTo(25));
+                Assert.That(BasicOperations.Handle('/', 49, 7), Is.EqualTo(7));
+            });
+        }
+    }
+}
