f57ed357f04a8897adc214a482d214feb40daf0a
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-05 23:11:46 -0300

    feat: BasicOperations

---
+++ Codewars/8-kyu/BasicOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+    public static class BasicOperations
+    {
+        public static double Handle(char operation, double value1, double value2)
+        {
+            return operation switch
+            {
+                '+' => value1 + value2,
+                '-' => value1 - value2,
+                '*' => value1 * value2,
+                '/' => value1 / value2,
+                _ => default,
+            };
+        }
+    }
+}
