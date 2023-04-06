79f7533b0cad0d7c5e90486503c494a6f6c1e952
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-05 22:34:13 -0300

    test: CountSheeps

---
+++ Codewars.Test/8-kyu/CountSheeps.cs
@@ -0,0 +1,17 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class CountSheepsTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            bool[] sheeps = new[] { true, false, true };
+
+            Assert.That(CountSheeps.Handle(sheeps), Is.EqualTo(2));
+        }
+    }
+}
