6bd117c177381d8cb033d31a74bb00d7a81695d1
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-11 22:46:08 -0300

    test: CenturyFromYear

---
+++ Codewars.Test/8-kyu/CenturyFromYear.cs
@@ -0,0 +1,21 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class CenturyFromYearTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(CenturyFromYear.Handle(1705), Is.EqualTo(18));
+                Assert.That(CenturyFromYear.Handle(1900), Is.EqualTo(19));
+                Assert.That(CenturyFromYear.Handle(1601), Is.EqualTo(17));
+                Assert.That(CenturyFromYear.Handle(2000), Is.EqualTo(20));
+            });
+        }
+    }
+}
