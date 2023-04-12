79c7116994147b4d6d87cb2ccd953a41f2f6e3fd
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-11 22:45:58 -0300

    feat: CenturyFromYear

---
+++ Codewars/8-kyu/CenturyFromYear.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+    public static class CenturyFromYear
+    {
+        public static int Handle(int year)
+        {
+            if (year % 100 == 0)
+            {
+                return year / 100;
+            }
+            else
+            {
+                return (year / 100) + 1;
+            }
+        }
+    }
+}
