79725fd32719e05e46ec13605bf962adfa3420e2
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-12 23:44:04 -0300

    feat: SquareDigits

---
+++ Codewars/7-kyu/SquareDigits.cs
@@ -0,0 +1,18 @@
+﻿namespace Codewars._7_kyu
+{
+    public static class SquareDigits
+    {
+        public static int Handle(int n)
+        {
+            string s = n.ToString();
+            string result = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += ((s[i] - '0') * (s[i] - '0')).ToString();
+            }
+
+            return int.Parse(result);
+        }
+    }
+}
