ab982dec129abbfd3fcc03817bad564aba6c2ede
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-12 23:15:10 -0300

    test: Disemvowel

---
+++ Codewars.Test/7-kyu/DisemvowelTest.cs
@@ -0,0 +1,20 @@
+﻿using Codewars._7_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._7_kyu
+{
+    [TestFixture]
+    public class DisemvowelTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(Disemvowel.Handle("This website is for losers LOL!"), Is.EqualTo("Ths wbst s fr lsrs LL!"));
+                Assert.That(Disemvowel.Handle("No offense but,\nYour writing is among the worst I've ever read"), Is.EqualTo("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"));
+                Assert.That(Disemvowel.Handle("What are you, a communist?"), Is.EqualTo("Wht r y,  cmmnst?"));
+            });
+        }
+    }
+}
