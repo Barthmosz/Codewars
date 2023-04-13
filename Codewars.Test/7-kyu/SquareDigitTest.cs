13ac089e8ecd002ce46f7477e90f2ccaf7d4b7dd
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-12 23:44:11 -0300

    test: SquareDigit

---
+++ Codewars.Test/7-kyu/SquareDigitTest.cs
@@ -0,0 +1,19 @@
+﻿using Codewars._7_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._7_kyu
+{
+    [TestFixture]
+    public class SquareDigitTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(SquareDigits.Handle(9119), Is.EqualTo(811181));
+                Assert.That(SquareDigits.Handle(0), Is.EqualTo(0));
+            });
+        }
+    }
+}
