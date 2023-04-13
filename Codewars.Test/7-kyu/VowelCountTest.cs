abca5ccfcf724ee13be868a4c821521de6a40a04
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-12 22:44:26 -0300

    test: VowelCount

---
+++ Codewars.Test/7-kyu/VowelCountTest.cs
@@ -0,0 +1,15 @@
+﻿using Codewars._7_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._7_kyu
+{
+    [TestFixture]
+    public class VowelCountTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.That(VowelCount.Handle("abracadabra"), Is.EqualTo(5));
+        }
+    }
+}
