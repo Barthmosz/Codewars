b3f8687bfa987139c589d71d20d7538418325f09
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-12 22:44:21 -0300

    feat: VowelCount

---
+++ Codewars/7-kyu/VowelCount.cs
@@ -0,0 +1,24 @@
+﻿namespace Codewars._7_kyu
+{
+    public static class VowelCount
+    {
+        public static int Handle(string str)
+        {
+            int vowelCount = 0;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    if (str[i] == vowels[j])
+                    {
+                        vowelCount++;
+                    }
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
