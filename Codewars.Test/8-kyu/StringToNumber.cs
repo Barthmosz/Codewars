c7bf147dc49b35433de9b46747aa5ad42f57270a
Barthmosz <barthmosz@gmail.com>
2023-07-07 20:50:45 -0300

    test: StringToNumber

---
+++ Codewars.Test/8-kyu/StringToNumber.cs
@@ -0,0 +1,21 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class StringToNumberTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(StringToNumber.Handle("1234"), Is.EqualTo(1234));
+                Assert.That(StringToNumber.Handle("605"), Is.EqualTo(605));
+                Assert.That(StringToNumber.Handle("1405"), Is.EqualTo(1405));
+                Assert.That(StringToNumber.Handle("-7"), Is.EqualTo(-7));
+            });
+        }
+    }
+}
