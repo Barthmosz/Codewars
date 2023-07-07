16e00dc4db8ca0ef7b60a4d266f76bf22914687c
Barthmosz <barthmosz@gmail.com>
2023-07-07 20:50:40 -0300

    feat: StringToNumber

---
+++ Codewars/8-kyu/StringToNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Codewars._8_kyu
+{
+    public static class StringToNumber
+    {
+        public static int Handle(string str)
+        {
+            int result = Convert.ToInt32(str);
+            return result;
+        }
+    }
+}
