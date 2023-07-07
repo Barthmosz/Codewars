ce324934a8ec78495f15d853616e1efaacb48a68
Barthmosz <barthmosz@gmail.com>
2023-07-07 20:44:35 -0300

    test: AbbreviateName

---
+++ Codewars.Test/8-kyu/AbbreviateName.cs
@@ -0,0 +1,22 @@
+﻿using Codewars._8_kyu;
+using NUnit.Framework;
+
+namespace Codewars.Test._8_kyu
+{
+    [TestFixture]
+    public class AbbreviateNameTest
+    {
+        [Test]
+        public void HandleTest()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(AbbreviateName.Handle("Sam Harris"), Is.EqualTo("S.H"));
+                Assert.That(AbbreviateName.Handle("Patrick Feenan"), Is.EqualTo("P.F"));
+                Assert.That(AbbreviateName.Handle("Evan Cole"), Is.EqualTo("E.C"));
+                Assert.That(AbbreviateName.Handle("P Favuzzi"), Is.EqualTo("P.F"));
+                Assert.That(AbbreviateName.Handle("David Mendieta"), Is.EqualTo("D.M"));
+            });
+        }
+    }
+}
