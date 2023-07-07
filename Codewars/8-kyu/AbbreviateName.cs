f1187c9c0ab89ecd616ae6078666d2f9506bdbbd
Barthmosz <barthmosz@gmail.com>
2023-07-07 20:44:28 -0300

    feat: AbbreviateName

---
+++ Codewars/8-kyu/AbbreviateName.cs
@@ -0,0 +1,17 @@
+﻿namespace Codewars._8_kyu
+{
+    public static class AbbreviateName
+    {
+        public static string Handle(string name)
+        {
+            string result = "";
+
+            char firstLetter = name[0];
+            char lastLetter = name[name.IndexOf(" ") + 1];
+            
+            result = $"{firstLetter}.{lastLetter}";
+
+            return result;
+        }
+    }
+}
